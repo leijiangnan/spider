7af35fc3cc1795f47d6ac1079426f1749d0f46cd
leijiang.7 <leijiang.7@bytedance.com>
2022-12-03 13:35:45 +0800

    utf8

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/leijiangnan/spider
 
 go 1.18
+
+require (
+	golang.org/x/net v0.2.0 // indirect
+	golang.org/x/text v0.4.0 // indirect
+)
--- main.go
+++ main.go
@@ -1,45 +1,51 @@
 package main
 
 import (
-	"bytes"
+	"bufio"
 	"fmt"
+	"golang.org/x/net/html/charset"
+	"golang.org/x/text/encoding"
+	"golang.org/x/text/encoding/unicode"
+	"golang.org/x/text/transform"
 	"io/ioutil"
 	"net/http"
-	"strings"
 )
 
 func main() {
 	url := "https://www.thepaper.cn/"
-	resp, err := http.Get(url)
-
+	body, err := Fetch(url)
 	if err != nil {
-		fmt.Printf("fetch url error:%v\n", err)
+		fmt.Printf("read content failed:%v\n", err)
 		return
+	}
+
+	fmt.Println(string(body))
+}
+
+func Fetch(url string) ([]byte, error) {
+	resp, err := http.Get(url)
+	if err != nil {
+		panic(err)
 	}
 
 	defer resp.Body.Close()
 
 	if resp.StatusCode != http.StatusOK {
-		fmt.Printf("Error status code:%v\n", resp.StatusCode)
-		return
+		fmt.Printf("Error status code:%d\n", resp.StatusCode)
 	}
+	bodyReader := bufio.NewReader(resp.Body)
+	e := DeterminEncoding(bodyReader)
+	utf8Reader := transform.NewReader(bodyReader, e.NewDecoder())
+	return ioutil.ReadAll(utf8Reader)
 
-	body, err := ioutil.ReadAll(resp.Body)
+}
 
+func DeterminEncoding(r *bufio.Reader) encoding.Encoding {
+	bytes, err := r.Peek(1024)
 	if err != nil {
-		fmt.Printf("read content failed:%v", err)
-		return
+		fmt.Printf("fetch error:%v\n", err)
+		return unicode.UTF8
 	}
-
-	numLinks := strings.Count(string(body), "<a")
-	fmt.Printf("homepage has %d links!\n", numLinks)
-
-	numLinks = bytes.Count(body, []byte("<a"))
-	fmt.Printf("homepage has %d links!\n", numLinks)
-
-	exist := strings.Contains(string(body), "疫情")
-	fmt.Printf("是否存在疫情:%v\n", exist)
-
-	exist = bytes.Contains(body, []byte("疫情"))
-	fmt.Printf("是否存在疫情:%v\n", exist)
+	e, _, _ := charset.DetermineEncoding(bytes, "")
+	return e
 }
